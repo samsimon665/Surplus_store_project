4206a71a422cc6e11ff85d536655de885f123e32
samsimon665 <samsimon665@gmail.com>
2026-05-01 21:21:16 +0530

    Compleated admin side order listing , movinng to order detail page

--- apps/adminpanel/urls.py
+++ apps/adminpanel/urls.py
@@ -15,6 +15,8 @@ from apps.adminpanel.views.products import product_list, product_create, product
 
 from apps.adminpanel.views.variants import variant_list, variant_create, variant_edit
 
+from apps.adminpanel.views.orders import orders
+
 from apps.adminpanel.views.faq import faq_list, faq_create
 
 from apps.adminpanel.views.promotions import promo_list, promo_create, promo_edit
@@ -55,6 +57,9 @@ urlpatterns = [
     path("products/<int:product_id>/variants/", variant_list, name="variant_list"),
     path("products/<int:product_id>/variants/create/", variant_create, name="variant_create"),
     path("products/<int:product_id>/variants/<int:variant_id>/edit/", variant_edit, name="variant_edit",),
+
+
+    path("orders/", orders, name="admin_orders"),
     
 
     path("faqs/", faq_list, name="faq_list"),
--- apps/adminpanel/views/orders.py
+++ apps/adminpanel/views/orders.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from apps.adminpanel.decorators import admin_required
+from apps.orders.models import Order
+
+from django.core.paginator import Paginator
+
+from django.db.models import Q
+
 
 
 @admin_required
 def orders(request):
-    return render(request, "adminpanel/orders.html")
+
+    qs = Order.objects.select_related("user").order_by("-created_at")
+
+    status = request.GET.get("status")
+
+    if status:
+        qs = qs.filter(status=status)
+
+    # Optional: quick filter for "unpaid"
+    if status == "unpaid":
+        qs = qs.filter(payment_status="pending")
+
+    paginator = Paginator(qs, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "adminpanel/orders/order_list.html", {
+        "orders": page_obj,
+        "page_obj": page_obj,
+        "paginator": paginator,
+        "current_status": status,
+    })
